4df5871caa19f4dc765bb335e36958254a295aa5
Rajesh Seshadri <seshadri.rajesh@gmail.com>
2020-07-19 23:25:43 -0400

    6 local

--- Hello.py
+++ Hello.py
@@ -2,5 +2,5 @@ msg = "This is a test from VSCode with GIT by dad" ;
 print(msg);
 
 // resolved
-print("another change to file - seq #5-Local Change");
+print("another change to file - seq #6 - Local Change");
 
